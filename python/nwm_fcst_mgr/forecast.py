848748b6bfba0447e0e61591617245f8453b7631
jeff.wade <jeff.s.wade@rtx.com>
2025-09-15 15:43:23 -0700

    Implement nwm-fcst-mgr compatible with new cold start and forecast workflow

---
+++ python/nwm_fcst_mgr/forecast.py
@@ -0,0 +1,226 @@
+import glob
+import json
+import logging
+import os
+import shutil
+import subprocess
+from pathlib import Path
+import geopandas as gpd
+import pandas as pd
+import netCDF4
+
+import matplotlib.pyplot as plt
+import yaml
+import argparse
+
+# from git_util import print_git_info_all
+from nwm_fcst_mgr.log_level import log_level_set
+
+# setup the logger
+log_level_set()
+logger = logging.getLogger(__name__)
+
+
+def run_fcst(valid_yaml: str, fcst_real: str, cold_start_real: str | None = None):
+    """
+    Execute ngen run for forecast period and cold start period (if provided)
+    """
+
+    # Read validation yaml file
+    valid_config = load_yaml(valid_yaml)
+
+    # Retrieve forecast output_dir
+    fcst_real_path = Path(fcst_real)
+    real_path_list = [fcst_real_path]
+    out_dir_list = [fcst_real_path.parent]
+
+    # Gather paths for cold start and forecast runs
+    real_path_list = [fcst_real_path]
+
+    # Load cold start realization file if it is provided
+    if cold_start_real:
+
+        # Retrieve cold_start output_dir
+        cold_start_real_path = Path(cold_start_real)
+        real_path_list.append(cold_start_real_path)
+        out_dir_list.append(cold_start_real_path.parent)
+
+    # Retrieve hydrofabric gpkg
+    gpkg_cats = valid_config['model']['catchments']
+    gpkg_nexus = valid_config['model']['nexus']
+
+    # Retrieve ngen executable
+    ngen_exe = valid_config['model']['binary']
+
+    # get gage ID and make sure it is not empty
+    try:
+        gage0 = valid_config['model']['eval_params']['basinID']
+    except ValueError as e:
+        logger.critical(f'Key model/eval_params/basinID not found in {valid_yaml}\n{e}')
+        raise
+    if gage0 == "":
+        try:
+            raise ValueError(f'basinID in {valid_yaml} cannot be empty')
+        except ValueError as e:
+            logger.critical(e)
+            raise
+
+    # Reverse realization and out_dir lists to process cold start first
+    real_path_list = list(reversed(real_path_list))
+    out_dir_list = list(reversed(out_dir_list))
+
+    print(f"real_path_list: {real_path_list}")
+    print(f"out_dir_list: {out_dir_list}")
+
+    # Execute ngen run for both cold-start (if it exists) and forecast period
+    for i in range(len(real_path_list)):
+
+        # Retrieve realization and output directory paths
+        real_path = real_path_list[i]
+        out_dir = out_dir_list[i]
+
+        # Run ngen
+        cmd = f'{ngen_exe} {gpkg_cats} "all" {gpkg_nexus} "all" {real_path}'
+
+        # kick off ngen run and save stdout & stderr to ngen_stdout_stderr.log
+        log_file = out_dir / "ngen_stdout_stderr.log"
+        with open(log_file, 'a+') as log:
+            subprocess.check_call(cmd, stdout=log, stderr=log, shell=True, cwd=str(out_dir))
+
+        # move output files to output directory
+        run_output_dir = out_dir / "output/"
+        run_output_dir.mkdir(parents=True, exist_ok=True)
+        for pat1 in ['cat*.csv', 'nex*.csv', 'troute*.nc']:
+            for f1 in glob.glob(f'{out_dir}/{pat1}'):
+                shutil.move(f1, Path(run_output_dir, os.path.basename(f1)))
+        if i == 0 and cold_start_real is not None:
+            logger.info(f'Cold start NGEN run outputs are saved at: {run_output_dir}')
+        else:
+            logger.info(f'Forecast NGEN run outputs are saved at: {run_output_dir}')
+
+        # read troute output file
+        outfile = glob.glob(f'{run_output_dir}/troute*.nc')[0]
+        output = read_troute_output(gage0, valid_config['model']['crosswalk'], gpkg_cats, outfile)
+
+        # plot the hydrograph
+        output.plot(y='sim_flow', kind='line')
+        plt.xlabel('Time')
+        plt.ylabel('Streamflow (m^3/s)')
+        plt.savefig(Path(run_output_dir, gage0 + '_hydrograph.png'), bbox_inches="tight")
+
+        # save streamflow simulation to csv
+        output.to_csv(Path(run_output_dir, gage0 + '_output.csv'))
+
+        if i == 0 and cold_start_real is not None:
+            logger.info("Cold start NGEN run completed.")
+        else:
+            logger.info("Forecast NGEN run completed.")
+
+
+def load_yaml(file_path: str) -> dict:
+    """
+    Read yaml-based configuration file from previous ngen calibration run
+    """
+    # Confirm input file exists
+    file_path = Path(file_path).absolute()
+    if not file_path.exists():
+        try:
+            raise FileNotFoundError(f'Input file not found: {file_path}')
+        except FileNotFoundError as e:
+            logger.critical(e)
+            raise
+
+    # Read the yaml-based configuration file
+    try:
+        with open(file_path) as file:
+            yaml_dict = yaml.safe_load(file)
+    except FileNotFoundError as e:
+        logger.critical(f'Config valid yaml file does not exist: {file_path}\n{e}')
+        raise
+    except yaml.YAMLError as e:
+        logger.critical(f"YAML parsing error in valid config yaml file: {file_path}\n{e}")
+        raise
+    except Exception as e:
+        logger.critical(f"Unexpected error loading valid config yaml file at: {file_path}\n{e}")
+        raise
+
+    return yaml_dict
+
+
+def read_troute_output(
+        gage0: str,
+        cwt_file: Path,
+        gpkg_file: Path,
+        out_file: Path,
+) -> pd.DataFrame:
+
+    """
+    Arguments:
+    ---------
+    gage0: gage ID to retrieve streamflow simulations
+    cwt_file: path to crosswalk file mapping gage to catchments
+    gpkg_file: path to geopackage file
+    out_file: path to t-route output file (in NetCDF format)
+
+    Returns:
+    ---------
+    dataframe containing time and streamflow simulations
+
+    """
+    # Handle crosswalk file (in order to get the correct feature_id when reading t-route data)
+    x_walk = pd.Series(dtype=object)
+    try:
+        with open(cwt_file) as fp:
+            data = json.load(fp)
+            for id, values in data.items():
+                gage = values.get('Gage_no')
+                if gage:
+                    if not isinstance(gage, str):
+                        gage = gage[0]
+                    if gage == gage0:
+                        x_walk[id] = gage
+                        break
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Crosswalk file '{cwt_file}' not found.")
+    except json.JSONDecodeError:
+        raise ValueError(f"Failed to parse JSON from crosswalk file '{cwt_file}'.")
+
+    if x_walk.empty:
+        raise Exception(f'{gage0} is not found in crosswalk file {cwt_file}')
+
+    # get catchment at basin outlet for reading from t-route output
+    catchment_hydro_fabric = gpd.read_file(gpkg_file, layer='divides')
+    catchment_hydro_fabric.set_index('id', inplace=True)
+    nexus_id = catchment_hydro_fabric.loc[x_walk.index[0].replace('cat', 'wb')]['toid']
+    wb_lst = [x.split('-')[1] for x in catchment_hydro_fabric.index[catchment_hydro_fabric['toid'] == nexus_id]]
+
+    # read troute output
+    ncvar = netCDF4.Dataset(out_file, "r")
+    fid_index = [list(ncvar['feature_id'][0:]).index(int(fid)) for fid in wb_lst]
+    output = pd.DataFrame(data={'sim_flow': pd.DataFrame(ncvar['flow'][fid_index], index=fid_index).T.sum(axis=1)})
+    t0 = pd.to_datetime(ncvar.file_reference_time, format="%Y-%m-%d_%H:%M:%S")
+    output.index = [t0 + pd.Timedelta(seconds=int(t1)) for t1 in ncvar['time']]
+    output.index.name = 'Time'
+
+    return output
+
+
+def parse_args():
+    # Create command line parser
+    parser = argparse.ArgumentParser()
+
+    # Add arguments
+    parser.add_argument('valid_yaml', type=str, help=('Path to validation yaml file from previous run of nwm-cal-mgr'))
+    parser.add_argument('fcst_real', type=str, help=('Path to forecast period realization file'))
+    parser.add_argument('cold_start_real', type=str, nargs="?", default=None, help=('Path to cold_start period realization file'))
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    run_fcst(args.valid_yaml, args.fcst_real, args.cold_start_real)
+
+
+if __name__ == "__main__":
+    main()
